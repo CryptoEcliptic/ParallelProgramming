83170d2796ed3c05b95361126d42ea7e135b588b
CryptoEcliptic <r.e.vassilev@gmail.com>
2020-12-23 17:48:55 +0200

    Added ManualResetEventSlim example

---
+++ 14.ManualResetEventSlimAndSemaphoreSlim/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _14.ManualResetEventSlimAndSemaphoreSlim
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Unlike barrier and countdown events where we have counter, here we have only one time event. 
+            //We set the signal on one lonaction and wait it on another location.
+            var evt = new ManualResetEventSlim();
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Boiling water...");
+                //Sets the signal
+                evt.Set();
+            });
+
+            var makeTea = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Waiting for water...");
+                //Wait the signal
+                evt.Wait();
+                Console.WriteLine("Here is your tea!");
+            });
+
+            makeTea.Wait();
+        }
+    }
+}
