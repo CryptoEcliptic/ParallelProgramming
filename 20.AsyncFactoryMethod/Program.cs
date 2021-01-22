33e3f732baf2b840574bc7994cfc17867ca5ccab
CryptoEcliptic <r.e.vassilev@gmail.com>
2021-01-22 08:30:32 +0200

    Added Async Factory pattern and AsyncInit pattern

---
+++ 20.AsyncFactoryMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+
+namespace _20.AsyncFactoryMethod
+{
+    //Since constructors cannot be called asynchroneously i.e. we cannot create an object async, this is a workaround.
+    public class Foo
+    {
+        //Private ctor not allowing the instance to be created outside
+        private Foo()
+        {
+
+        }
+
+        //Private async method returning the object
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+
+        //Public method accessed from outside wich returns new instance of the class
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Foo fooAsync = await Foo.CreateAsync();
+        }
+    }
+}
---
+++ 21.AsyncInitializationPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _21.AsyncInitializationPattern
+{
+    //For DI container Interface could be used for async initialization.
+    public interface IAsyncInit
+    {
+        Task InitializeTask { get; }
+    }
+
+    public class MyClass : IAsyncInit
+    {
+        public MyClass()
+        {
+            InitializeTask = InitalizeAsync();
+        }
+
+        public Task InitializeTask { get; }
+
+        private async Task InitalizeAsync()
+        {
+            await Task.Delay(1000);
+        }
+    
+    }
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var myClass = new MyClass();
+
+            if (myClass is IAsyncInit ia)
+            {
+                await ia.InitializeTask;
+            }
+        }
+    }
+}
